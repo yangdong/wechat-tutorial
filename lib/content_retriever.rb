3a83fd9c1836d31bbe00c6cb04b5a6ffffab7d12
Yang Dong <don.yangdong@gmail.com>
2013-09-29 15:51:07 +0800

    update title

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -6,7 +6,7 @@ class ContentRetriver
 		{
 			total: 1,
 			contents: [{
-				title: '这是预制在该公众账号中的图文消息',
+				title: '简单几步成为微信公众平台开发者',
 				description: '该公众账户是为了演示如何成为微信平台的开发者，如果想要了解更详细的内容，请点击该消息。',
 				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
 				url: 'http://linkedme.in/2013/09/25/turoial-to-be-wechat-public-account-developer/'}]
