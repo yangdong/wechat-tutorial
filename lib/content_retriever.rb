244b5289fb12b2da8789876f9db2855acdb0d635
ljm625 <ljm625@gmail.com>
2014-03-10 23:17:08 +0800

    Update content_retriever.rb

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -6,10 +6,9 @@ class ContentRetriver
 		{
 			total: 1,
 			contents: [{
-				title: '简单几步成为微信公众平台开发者',
-				description: '该公众账户是为了演示如何成为微信平台的开发者，如果想要了解更详细的内容，请点击该消息。',
-				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
-				url: 'http://linkedme.in/2013/09/25/turoial-to-be-wechat-public-account-developer/'}]
+				title: '测试',
+				description: '这段是自动应答的 测试',
+}]
 		}
 	end
 end
