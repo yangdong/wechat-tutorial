7156e72e5905cb7589e6e8c0cb6acf5f9d7bc6d3
ljm625 <ljm625@gmail.com>
2014-03-11 10:19:58 +0800

    Update dispatcher.rb

--- lib/dispatcher.rb
+++ lib/dispatcher.rb
@@ -3,6 +3,6 @@ class Dispatcher
 		return :help if not message_info[:content].nil? and message_info[:content].include? 'help'
 		return :advance if not message_info[:content].nil? and message_info[:content].include? '阙浩然'
 		return :welcome if not message_info[:event].nil? and message_info[:event] == :subscribe
-		:picture_message
+		
 	end
 end
